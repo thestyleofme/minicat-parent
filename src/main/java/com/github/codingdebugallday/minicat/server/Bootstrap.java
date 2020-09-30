a2b7f0b5fc5643e93d788396d49db5bab8021f6c
isaac <codingdebugallday@163.com>
2020-10-01 02:13:48 +0800

    :sparkles: v2.0

--- src/main/java/com/github/codingdebugallday/minicat/server/Bootstrap.java
+++ src/main/java/com/github/codingdebugallday/minicat/server/Bootstrap.java
@@ -1,12 +1,10 @@
 package com.github.codingdebugallday.minicat.server;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-
-import com.github.codingdebugallday.minicat.server.utils.HttpProtocolUtil;
 
 /**
  * <p>
@@ -38,19 +36,18 @@ public class Bootstrap {
      */
     public void start() throws IOException {
         //===============================================================================
-        // 完成minicat 1.0版本
-        // 需求：浏览器请求http://localhost:8080，返回一个固定的字符串到页面 "hello Minicat!"
+        // 完成minicat 2.0版本
+        // 需求：封装Request和Response对象，返回html静态资源文件
         //===============================================================================
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("============>>> Minicat start on port: " + port);
         while (true) {
             Socket socket = serverSocket.accept();
+            InputStream inputStream = socket.getInputStream();
+            Request request = new Request(inputStream);
             OutputStream outputStream = socket.getOutputStream();
-            String data = "hello Minicat!";
-            String httpHeader200 = HttpProtocolUtil.getHttpHeader200(data.getBytes(StandardCharsets.UTF_8).length);
-            // String httpHeader200 = HttpProtocolUtil.getHttpHeader404();
-            String responseText = httpHeader200 + data;
-            outputStream.write(responseText.getBytes(StandardCharsets.UTF_8));
+            Response response = new Response(outputStream);
+            response.outputHtml(request.getUrl());
             socket.close();
         }
     }
---
+++ src/main/java/com/github/codingdebugallday/minicat/server/utils/StaticResourceUtil.java
@@ -0,0 +1,56 @@
+package com.github.codingdebugallday.minicat.server.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * <p>
+ * description
+ * </p>
+ *
+ * @author isaac 2020/10/01 1:28
+ * @since 1.0.0
+ */
+public class StaticResourceUtil {
+
+    private StaticResourceUtil() {
+    }
+
+    /**
+     * 获取静态资源的绝对路径
+     *
+     * @param path path
+     * @return absolute path
+     */
+    public static String getAbsolutePath(String path) {
+        String absolutePath = StaticResourceUtil.class.getResource("/").getPath();
+        // 去点末尾的/
+        if (absolutePath.endsWith("/")) {
+            absolutePath = absolutePath.substring(0, absolutePath.length() - 1);
+        }
+        return absolutePath.replaceAll("\\\\", "/") + path;
+    }
+
+    /**
+     * 读取静态资源文件输入流，通过输出流输出
+     *
+     * @param inputStream  InputStream
+     * @param outputStream OutputStream
+     */
+    public static void outputStaticResource(InputStream inputStream, OutputStream outputStream) throws IOException {
+        byte[] bytes = new byte[1024];
+        StringBuilder stringBuilder = new StringBuilder();
+        while (inputStream.read(bytes) != -1) {
+            stringBuilder.append(new String(bytes));
+        }
+        String inputStr = stringBuilder.toString();
+        // 输出http请求头
+        outputStream.write(HttpProtocolUtil.getHttpHeader200(
+                inputStr.getBytes(StandardCharsets.UTF_8).length)
+                .getBytes(StandardCharsets.UTF_8));
+        // 输出具体内容
+        outputStream.write(inputStr.getBytes(StandardCharsets.UTF_8));
+    }
+}
