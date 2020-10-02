b86726f09a6d5dd4a79a11ae562293258f5929e9
isaac <codingdebugallday@163.com>
2020-10-03 01:18:25 +0800

    :sparkles: v3.0

--- src/main/java/com/github/codingdebugallday/minicat/server/Bootstrap.java
+++ src/main/java/com/github/codingdebugallday/minicat/server/Bootstrap.java
@@ -1,10 +1,19 @@
 package com.github.codingdebugallday.minicat.server;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.github.codingdebugallday.minicat.server.servlet.HttpServlet;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
 /**
  * <p>
@@ -26,7 +35,7 @@ public class Bootstrap {
         try {
             // 启动
             bootstrap.start();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -34,11 +43,14 @@ public class Bootstrap {
     /**
      * minicat启动初始化操作
      */
-    public void start() throws IOException {
+    public void start() throws Exception {
         //===============================================================================
-        // 完成minicat 2.0版本
-        // 需求：封装Request和Response对象，返回html静态资源文件
+        // 完成minicat 3.0版本
+        // 需求：封装Request和Response对象，返回html静态资源文件，还可以请求动态资源（Servlet）
         //===============================================================================
+
+        // 加载解析相关的配置，web.xml
+        loadServlet();
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("============>>> Minicat start on port: " + port);
         while (true) {
@@ -47,8 +59,48 @@ public class Bootstrap {
             Request request = new Request(inputStream);
             OutputStream outputStream = socket.getOutputStream();
             Response response = new Response(outputStream);
-            response.outputHtml(request.getUrl());
+            String url = request.getUrl();
+            if (!serverMap.containsKey(url)) {
+                // 静态资源处理
+                response.outputHtml(url);
+            } else {
+                // 动态资源（servlet）处理
+                HttpServlet httpServlet = serverMap.get(url);
+                httpServlet.service(request, response);
+            }
             socket.close();
+        }
+    }
+
+    private final Map<String, HttpServlet> serverMap = new HashMap<>();
+
+    /**
+     * 加载解析web.xml，初始化Servlet
+     */
+    private void loadServlet() {
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("web.xml");
+        SAXReader saxReader = new SAXReader();
+        try {
+            Document document = saxReader.read(resourceAsStream);
+            Element rootElement = document.getRootElement();
+            List<Element> servletElementList = rootElement.elements("servlet");
+            for (Element servletElement : servletElementList) {
+                Element servletNameElement = servletElement.element("servlet-name");
+                String servletName = servletNameElement.getStringValue();
+                Element servletClassElement = servletElement.element("servlet-class");
+                String servletClass = servletClassElement.getStringValue();
+                // 根据servlet-name的值找到url-pattern
+                Element servletMappingElement = (Element) rootElement.selectSingleNode(
+                        String.format("/web-app/servlet-mapping[servlet-name='%s']", servletName));
+                String urlPattern = servletMappingElement.element("url-pattern").getStringValue();
+                serverMap.put(urlPattern, (HttpServlet) Class.forName(servletClass)
+                        .getClassLoader()
+                        .loadClass(servletClass)
+                        .getDeclaredConstructor()
+                        .newInstance());
+            }
+        } catch (DocumentException | ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+            e.printStackTrace();
         }
     }
 
---
+++ src/main/java/com/github/codingdebugallday/minicat/server/servlet/Servlet.java
@@ -0,0 +1,39 @@
+package com.github.codingdebugallday.minicat.server.servlet;
+
+import com.github.codingdebugallday.minicat.server.Request;
+import com.github.codingdebugallday.minicat.server.Response;
+
+/**
+ * <p>
+ * description
+ * </p>
+ *
+ * @author isaac 2020/10/02 23:55
+ * @since 1.0.0
+ */
+public interface Servlet {
+
+    /**
+     * init
+     *
+     * @throws Exception e
+     */
+    void init() throws Exception;
+
+    /**
+     * destroy
+     *
+     * @throws Exception e
+     */
+    void destroy() throws Exception;
+
+    /**
+     * service
+     *
+     * @param request  Request
+     * @param response Response
+     * @throws Exception e
+     */
+    void service(Request request, Response response) throws Exception;
+
+}
